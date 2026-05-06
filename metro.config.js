09ed11293cadf085eb25f2d8808878033f53e280
Krebsix <krebsiandreas@gmail.com>
2026-05-06 20:07:42 +0800

    checkpoint: app running in Expo Go after SDK 54 fix

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,5 @@
-const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
-
-config.resolver.nodeModulesPaths = [path.resolve(__dirname, 'node_modules')];
-config.resolver.disableHierarchicalLookup = true;
 
 module.exports = config;
